083ccfcee1d6909f0f06362f074ccf1d2530352d
ashutosh9877 <sharmaashutosh36790@gmail.com>
2025-09-26 15:52:59 +0530

    Difference Between print,printf,println

---
+++ src/Lab_01_PrintF.java
@@ -0,0 +1,10 @@
+public class Lab_01_PrintF {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        System.out.println("Value of a is" + a);
+        System.out.printf("Value of b is" + b);
+        System.out.print("Sum of two number is" + a+b);
+    }
+}
