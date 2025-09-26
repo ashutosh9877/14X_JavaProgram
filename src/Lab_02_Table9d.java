d971a91f18046d3ec6fb477eceec46ef221a1540
ashutosh9877 <sharmaashutosh36790@gmail.com>
2025-09-26 16:24:53 +0530

    Table of 10 with %d

---
+++ src/Lab_02_Table9d.java
@@ -0,0 +1,16 @@
+public class Lab_02_Table9d {
+    public static void main(String[] args) {
+        int table = 10;
+
+        System.out.printf("%d * 1 = %d \n", table, table*1);
+        System.out.printf("%d * 2 = %d \n", table, table*2);
+        System.out.printf("%d * 3 = %d \n", table, table*3);
+        System.out.printf("%d * 4 = %d \n", table, table*4);
+        System.out.printf("%d * 5 = %d \n", table, table*5);
+        System.out.printf("%d * 6 = %d \n", table, table*6);
+        System.out.printf("%d * 7 = %d \n", table, table*7);
+        System.out.printf("%d * 8 = %d \n", table, table*8);
+        System.out.printf("%d * 9 = %d \n", table, table*9);
+        System.out.printf("%d * 10 = %d \n", table, table*10);
+    }
+}
