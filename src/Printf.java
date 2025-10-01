a6e2ca46134138c840c90baaede637b5fea2eff3
ashutosh9877 <sharmaashutosh36790@gmail.com>
2025-10-01 15:21:50 +0530

    Difference in Printf and Println

---
+++ src/Printf.java
@@ -0,0 +1,7 @@
+public class Printf {
+    public static void main(String[] args) {
+        System.out.printf("Name :%s, Rollno:%d","Ashutosh",24);  //here %s is String and %d is Decimal
+        System.out.println("Hello World!");
+        System.out.print("Hello World!");
+    }
+}
