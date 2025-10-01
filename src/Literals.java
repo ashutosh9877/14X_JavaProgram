df0511bd3997c892d198b34e4a9e9872a3edeb6f
ashutosh9877 <sharmaashutosh36790@gmail.com>
2025-10-01 15:55:36 +0530

    Literals

---
+++ src/Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+        int a = 12; // 12 is a int literal
+        byte b = 3; // 3 is the int Literal
+
+        String name = "Ashutosh Sharma";  //String Literals
+
+        float f = 22.34f;   // Float literals
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(f);
+    }
+}
