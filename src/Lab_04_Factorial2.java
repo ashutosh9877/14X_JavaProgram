db28522d79bffbc36a10945c325b2579c8a017d2
ashutosh9877 <sharmaashutosh36790@gmail.com>
2025-10-06 09:50:43 +0530

    Ternary Assignment 21

---
+++ src/Lab_04_Factorial2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Lab_04_Factorial2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Number");
+
+        int n = 1;
+        int Factorial = 1;
+
+        if (s.hasNextInt()) {
+            n = s.nextInt();
+        } else {
+            System.out.println("Enter the Integer value only");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Factorial *= i;
+        }
+
+        System.out.println("Factorial of " + n + " is " + Factorial);
+    }
+}
